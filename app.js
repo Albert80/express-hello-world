d27e4fd6e0701f69d03e59892333d54b8165c8bf
Albert80 <cesaratj27@gmail.com>
2018-03-30 10:32:25 -0600

    Anexando js y json's

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+// course at edx.org from Microsoft
+// Introduction to Node.js
+// Express Frame work
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
+app.listen(port)
